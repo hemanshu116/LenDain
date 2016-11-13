a2ead802a320b6af6f01961e25e600e607d1daa2
Hemanshu Solanki <hemanshu116@gmail.com>
2016-11-14 00:45:40 +0530

    Homepage for len Dain
    The css and the homepage of len Dain

---
+++ src/pojo/LoginPojo.java
@@ -0,0 +1,41 @@
+package pojo;
+
+public class LoginPojo {
+	private String name;
+	private String lastName;
+	private String password;
+	private String emailId;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
